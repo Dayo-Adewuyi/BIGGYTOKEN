947b43708b75cc1dfa067a5b033f02ba5ac2e064
dayolovesruby <dayoadewuyi@protonmail.com>
2022-03-20 14:00:34 +0100

    god abeg

--- contracts/Biggytoken.sol
+++ contracts/Biggytoken.sol
@@ -1,398 +1,122 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.0;
 
-import "./Ownable.sol";
-import "./Stakeable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-/**
- * @notice DevToken is a development token that we use to learn how to code solidity
- * and what BEP-20 interface requires
- */
-contract Biggytoken is Ownable, Stakeable {
-    /**
-     * @notice Our Tokens required variables that are needed to operate everything
-     */
-    uint256 public _totalSupply;
-    uint8 public _decimals;
-    string public _symbol;
-    string public _name;
+contract Biggytoken is ERC20, Ownable {
+    uint256 constant _initial_supply = 1000;
+    address[] internal stakeholders;
+    mapping(address => uint256) internal stakes;
+    mapping(address => uint256) internal rewards;
+    uint256 public tokenPricePerEther;
+    uint256 public stakingDuration;
+    mapping(address => uint256) internal stakingStartTime;
+    uint256 public tokensPerEth;
 
-    /**
-     * @notice _balances is a mapping that contains a address as KEY
-     * and the balance of the address as the value
-     */
-    mapping(address => uint256) private _balances;
-    /**
-     * @notice _allowances is used to manage and control allownace
-     * An allowance is the right to use another accounts balance, or part of it
-     */
-    mapping(address => mapping(address => uint256)) private _allowances;
-
-    /**
-     * @notice Events are created below.
-     * Transfer event is a event that notify the blockchain that a transfer of assets has taken place
-     *
-     */
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    /**
-     * @notice Approval is emitted when a new Spender is approved to spend Tokens on
-     * the Owners account
-     */
-    event Approval(
-        address indexed owner,
-        address indexed spender,
-        uint256 value
-    );
-
-    /**
-     * @notice constructor will be triggered when we create the Smart contract
-     * _name = name of the token
-     * _short_symbol = Short Symbol name for the token
-     * token_decimals = The decimal precision of the Token, defaults 18
-     * _totalSupply is how much Tokens there are totally
-     */
-    address private admin;
-    uint256 public end;
-
-    constructor() {
-        _name = "Biggytoken";
-        _symbol = "BGT";
-        _decimals = 18;
-        _totalSupply = 1000;
-
-        // Add all the tokens created to the creator of the token
-        _balances[msg.sender] = _totalSupply;
-        end = block.timestamp;
-        admin = msg.sender;
-
-        // Emit an Transfer event to notify the blockchain that an Transfer has occured
-        emit Transfer(address(0), msg.sender, _totalSupply);
+    constructor() ERC20("Biggytoken", "BGT") {
+        _mint(msg.sender, _initial_supply);
+        tokensPerEth = 1000;
+        stakingDuration = 7 days;
     }
 
-    /**
-     * @notice decimals will return the number of decimal precision the Token is deployed with
-     */
-    function decimals() external view returns (uint8) {
-        return _decimals;
-    }
-
-    /**
-     * @notice symbol will return the Token's symbol
-     */
-    function symbol() external view returns (string memory) {
-        return _symbol;
-    }
-
-    /**
-     * @notice name will return the Token's symbol
-     */
-    function name() external view returns (string memory) {
-        return _name;
-    }
-
-    /**
-     * @notice totalSupply will return the tokens total supply of tokens
-     */
-    function totalSupply() external view returns (uint256) {
-        return _totalSupply;
-    }
-
-    /**
-     * @notice balanceOf will return the account balance for the given account
-     */
-    function balanceOf(address account) external view returns (uint256) {
-        return _balances[account];
-    }
-
-    /**
-     * @notice _mint will create tokens on the address inputted and then increase the total supply
-     *
-     * It will also emit an Transfer event, with sender set to zero address (adress(0))
-     *
-     * Requires that the address that is recieveing the tokens is not zero address
-     */
-    function _mint(address account, uint256 amount) internal {
-        require(
-            account != address(0),
-            "biggyToken: cannot mint to zero address"
-        );
-
-        // Increase total supply
-        _totalSupply = _totalSupply + (amount);
-        // Add amount to the account balance using the balance mapping
-        _balances[account] = _balances[account] + amount;
-        // Emit our event to log the action
-        emit Transfer(address(0), account, amount);
-    }
-
-    /**
-     * @notice _burn will destroy tokens from an address inputted and then decrease total supply
-     * An Transfer event will emit with receiever set to zero address
-     *
-     * Requires
-     * - Account cannot be zero
-     * - Account balance has to be bigger or equal to amount
-     */
-    function _burn(address account, uint256 amount) internal {
-        require(
-            account != address(0),
-            "biggytoken: cannot burn from zero address"
-        );
-        require(
-            _balances[account] >= amount,
-            "biggytoken: Cannot burn more than the account owns"
-        );
-
-        // Remove the amount from the account balance
-        _balances[account] = _balances[account] - amount;
-        // Decrease totalSupply
-        _totalSupply = _totalSupply - amount;
-        // Emit event, use zero address as reciever
-        emit Transfer(account, address(0), amount);
-    }
-
-    /**
-     * @notice burn is used to destroy tokens on an address
-     *
-     * See {_burn}
-     * Requires
-     *   - msg.sender must be the token owner
-     *
-     */
-    function burn(address account, uint256 amount)
+    function isStakeholder(address _address)
         public
-        onlyOwner
-        returns (bool)
-    {
-        _burn(account, amount);
-        return true;
-    }
-
-    /**
-     * @notice mint is used to create tokens and assign them to msg.sender
-     *
-     * See {_mint}
-     * Requires
-     *   - msg.sender must be the token owner
-     *
-     */
-    function mint(address account, uint256 amount)
-        public
-        onlyOwner
-        returns (bool)
-    {
-        _mint(account, amount);
-        return true;
-    }
-
-    /**
-     * @notice transfer is used to transfer funds from the sender to the recipient
-     * This function is only callable from outside the contract. For internal usage see
-     * _transfer
-     *
-     * Requires
-     * - Caller cannot be zero
-     * - Caller must have a balance = or bigger than amount
-     *
-     */
-    function transfer(address recipient, uint256 amount)
-        external
-        returns (bool)
-    {
-        _transfer(msg.sender, recipient, amount);
-        return true;
-    }
-
-    /**
-     * @notice _transfer is used for internal transfers
-     *
-     * Events
-     * - Transfer
-     *
-     * Requires
-     *  - Sender cannot be zero
-     *  - recipient cannot be zero
-     *  - sender balance most be = or bigger than amount
-     */
-    function _transfer(
-        address sender,
-        address recipient,
-        uint256 amount
-    ) internal {
-        require(sender != address(0), "biggytoken: transfer from zero address");
-        require(
-            recipient != address(0),
-            "biggytoken: transfer to zero address"
-        );
-        require(
-            _balances[sender] >= amount,
-            "biggytoken: cant transfer more than your account holds"
-        );
-
-        _balances[sender] = _balances[sender] - amount;
-        _balances[recipient] = _balances[recipient] + amount;
-
-        emit Transfer(sender, recipient, amount);
-    }
-
-    /**
-     * @notice getOwner just calls Ownables owner function.
-     * returns owner of the token
-     *
-     */
-    function getOwner() external view returns (address) {
-        return owner();
-    }
-
-    /**
-     * @notice allowance is used view how much allowance an spender has
-     */
-    function allowance(address owner, address spender)
-        external
         view
-        onlyAdmin
-        returns (uint256)
+        returns (bool, uint256)
     {
-        return _allowances[owner][spender];
+        for (uint256 s = 0; s < stakeholders.length; s += 1) {
+            if (_address == stakeholders[s]) return (true, s);
+        }
+        return (false, 0);
     }
 
-    /**
-     * @notice approve will use the senders address and allow the spender to use X amount of tokens on his behalf
-     */
-    function approve(address spender, uint256 amount)
-        external
-        onlyAdmin
-        returns (bool)
-    {
-        _approve(msg.sender, spender, amount);
-        return true;
+    function addStakeholder(address _stakeholder) public onlyOwner {
+        (bool _isStakeholder, ) = isStakeholder(_stakeholder);
+        if (!_isStakeholder) stakeholders.push(_stakeholder);
     }
 
-    /**
-     * @notice _approve is used to add a new Spender to a Owners account
-     *
-     * Events
-     *   - {Approval}
-     *
-     * Requires
-     *   - owner and spender cannot be zero address
-     */
-    function _approve(
-        address owner,
-        address spender,
-        uint256 amount
-    ) internal onlyAdmin {
+    function removeStakeholder(address _stakeholder) public onlyOwner {
+        (bool _isStakeholder, uint256 s) = isStakeholder(_stakeholder);
+        if (_isStakeholder) {
+            stakeholders[s] = stakeholders[stakeholders.length - 1];
+            stakeholders.pop();
+        }
+    }
+
+    function stakeOf(address _stakeholder) public view returns (uint256) {
+        return stakes[_stakeholder];
+    }
+
+    function totalStakes() public view returns (uint256) {
+        uint256 _totalStakes = 0;
+        for (uint256 s = 0; s < stakeholders.length; s += 1) {
+            _totalStakes += stakes[stakeholders[s]];
+        }
+        return _totalStakes;
+    }
+
+    function createStake(uint256 _stake) public {
+        _burn(msg.sender, _stake);
+        if (stakes[msg.sender] == 0) {
+            addStakeholder(msg.sender);
+        }
+        stakes[msg.sender] += _stake;
+        stakingStartTime[msg.sender] = block.timestamp;
+    }
+
+    modifier UptoAWeek() {
         require(
-            owner != address(0),
-            "biggytoken: approve cannot be done from zero address"
+            block.timestamp > stakingStartTime[msg.sender] + 7 days,
+            "Not upto 1 week yet"
         );
-        require(
-            spender != address(0),
-            "biggytoken: approve cannot be to zero address"
-        );
-        // Set the allowance of the spender address at the Owner mapping over accounts to the amount
-        _allowances[owner][spender] = amount;
-
-        emit Approval(owner, spender, amount);
-    }
-
-    /**
-     * @notice transferFrom is uesd to transfer Tokens from a Accounts allowance
-     * Spender address should be the token holder
-     *
-     * Requires
-     *   - The caller must have a allowance = or bigger than the amount spending
-     */
-    function transferFrom(
-        address spender,
-        address recipient,
-        uint256 amount
-    ) external returns (bool) {
-        // Make sure spender is allowed the amount
-        require(
-            _allowances[spender][msg.sender] >= amount,
-            "biggytoken: You cannot spend that much on this account"
-        );
-        // Transfer first
-        _transfer(spender, recipient, amount);
-        // Reduce current allowance so a user cannot respend
-        _approve(
-            spender,
-            msg.sender,
-            _allowances[spender][msg.sender] - amount
-        );
-        return true;
-    }
-
-    /**
-     * @notice increaseAllowance
-     * Adds allowance to a account from the function caller address
-     */
-    function increaseAllowance(address spender, uint256 amount)
-        public
-        onlyAdmin
-        returns (bool)
-    {
-        _approve(
-            msg.sender,
-            spender,
-            _allowances[msg.sender][spender] + amount
-        );
-        return true;
-    }
-
-    /**
-     * @notice decreaseAllowance
-     * Decrease the allowance on the account inputted from the caller address
-     */
-    function decreaseAllowance(address spender, uint256 amount)
-        public
-        onlyAdmin
-        returns (bool)
-    {
-        _approve(
-            msg.sender,
-            spender,
-            _allowances[msg.sender][spender] - amount
-        );
-        return true;
-    }
-
-    modifier onlyAdmin() {
-        require(msg.sender == admin);
         _;
     }
 
-    /**
-     * Add functionality like burn to the _stake afunction
-     *
-     */
-    function stake(uint256 _amount) public {
-        // Make sure staker actually is good for it
-        require(
-            _amount < _balances[msg.sender],
-            "biggytoken: Cannot stake more than you own"
-        );
-
-        _stake(_amount);
-        // Burn the amount of tokens on the sender
-        _burn(msg.sender, _amount);
+    function removeStake(uint256 _stake) public {
+        stakes[msg.sender] -= _stake;
+        if (stakes[msg.sender] == 0) removeStakeholder(msg.sender);
+        _mint(msg.sender, _stake);
     }
 
-    /**
-     * @notice withdrawStake is used to withdraw stakes from the account holder
-     */
-    function withdrawStake(uint256 amount, uint256 stake_index) public {
-        require(
-            block.timestamp > (end + 7 days),
-            "you can only withdraw after a week boss"
-        );
-        uint256 amount_to_mint = _withdrawStake(amount, stake_index);
-        // Return staked tokens to user
-        _mint(msg.sender, amount_to_mint);
+    function rewardOf(address _stakeholder) public view returns (uint256) {
+        return rewards[_stakeholder];
     }
 
-    uint256 public tokensPerEth = 10;
+    function totalRewards() public view returns (uint256) {
+        uint256 _totalRewards = 0;
+        for (uint256 s = 0; s < stakeholders.length; s += 1) {
+            _totalRewards += rewards[stakeholders[s]];
+        }
+        return _totalRewards;
+    }
+
+    function calculateReward(address _stakeholder)
+        public
+        view
+        returns (uint256)
+    {
+        return stakes[_stakeholder] / 100;
+    }
+
+    function claimRewards() public UptoAWeek {
+        uint256 reward = calculateReward(msg.sender);
+        rewards[msg.sender] += reward;
+    }
+
+    function distributeRewards() public onlyOwner {
+        for (uint256 s = 0; s < stakeholders.length; s += 1) {
+            address stakeholder = stakeholders[s];
+            uint256 reward = calculateReward(stakeholder);
+            rewards[stakeholder] += reward;
+        }
+    }
+
+    function withdrawReward() public {
+        uint256 reward = rewards[msg.sender];
+        rewards[msg.sender] = 0;
+        _mint(msg.sender, reward);
+    }
 
     function buytoken(address receiver) public payable {
         require(msg.value > 0, "our tokens are 100/eth boss");
